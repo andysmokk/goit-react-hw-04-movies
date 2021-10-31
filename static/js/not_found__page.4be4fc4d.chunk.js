19bd2a6981041fd56454ec9a3e1fa6d8471eddd4
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-10-31 23:53:40 +0100

    Updates

--- static/js/not_found__page.4be4fc4d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[6],{38:function(t,e,i){"use strict";i.r(e),i.d(e,"default",(function(){return c}));var n=i(1);function c(){return Object(n.jsx)("h1",{children:"404 \u0421\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u0430"})}}}]);
-//# sourceMappingURL=not_found__page.4be4fc4d.chunk.js.map
